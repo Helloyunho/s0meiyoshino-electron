92b8e9a4fb0b78abfbc7fd5e483dc40c885cf045
Helloyunho <helloyunho@Helloyunhoui-iMac.local>
2019-05-29 17:05:16 +0900

    SECONDDDDDD

--- src/App.js
+++ src/App.js
@@ -4,22 +4,53 @@ import {
   TopAppBarRow,
   TopAppBarSection,
   TopAppBarNavigationIcon,
-  TopAppBarTitle
+  TopAppBarTitle,
+  Drawer,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerSubtitle,
+  DrawerContent,
+  List,
+  ListItem,
+  Icon,
+  Typography
 } from 'rmwc'
 import 'material-components-web/dist/material-components-web.min.css'
+import './App.css'
 
 const App = () => {
+  const [drawerOpened, setDrawerOpened] = React.useState(false)
   return (
-    <div className='App'>
+    <React.Fragment className='App'>
+      <Drawer modal open={drawerOpened} onClose={() => setDrawerOpened(false)}>
+        <DrawerHeader>
+          <DrawerTitle>s0meiyoshino electron</DrawerTitle>
+          <DrawerSubtitle>Made ❤️ with Helloyunho</DrawerSubtitle>
+        </DrawerHeader>
+        <DrawerContent>
+          <List>
+            <ListItem>
+              <Icon icon='home' />Homf
+            </ListItem>
+            <ListItem>
+              <Icon icon='info' />Info
+            </ListItem>
+          </List>
+        </DrawerContent>
+      </Drawer>
+
       <TopAppBar short>
         <TopAppBarRow>
           <TopAppBarSection>
-            <TopAppBarNavigationIcon icon='menu' />
+            <TopAppBarNavigationIcon icon='menu' onClick={() => setDrawerOpened(true)} />
             <TopAppBarTitle>asdfasdf</TopAppBarTitle>
           </TopAppBarSection>
         </TopAppBarRow>
       </TopAppBar>
-    </div>
+      <div className='main'>
+        <h1>FUCK YOU</h1>
+      </div>
+    </React.Fragment>
   )
 }
 
